7138518f74995e2b855e3512e3a7ff68d69f8484
klssstis <140957188+klssstis@users.noreply.github.com>
2023-10-08 19:41:45 +0300

    Update worker1.py

--- worker1.py
+++ worker1.py
@@ -79,8 +79,8 @@ def depTocsv(fileNamePOM,filenameCSV):
 
     with open(filenameCSV, 'w') as outcsv:
         writer = csv.writer(outcsv, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL, lineterminator='\n')
-        print(listCSV)
         for item in listCSV:
+            print(item)
             writer.writerow([item[0], item[1], item[2]])
     return orCountTMP
 
