0019aa2025fa1c6e801fda097ac7c68b3a336cd0
klssstis <140957188+klssstis@users.noreply.github.com>
2023-10-20 11:08:40 +0300

    Update worker1.py

--- worker1.py
+++ worker1.py
@@ -18,8 +18,8 @@ except IndexError:
 os.system('echo '+token+'| gh auth login -h github.com --with-token')
 
 t = datetime.datetime.today()
-repoCSV = './results/repo_'+t.strftime('%Y%m%d')+'.csv'
-depCSV = './results/dep_'+t.strftime('%Y%m%d')+'.csv'
+repoCSV = './results/repo_'+t.strftime('%Y%m%d')+'_h.csv'
+depCSV = './results/dep_'+t.strftime('%Y%m%d')+'_h.csv'
 repoCSVup = './results/repo_now_h.csv'
 depCSVup = './results/dep_now_h.csv'
 if os.path.exists(repoCSVup) and os.path.exists(depCSVup):
